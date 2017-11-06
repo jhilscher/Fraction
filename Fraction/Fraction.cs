8bcbdd07c309aeefa9730405fd78b9b93a950c4b
jHilscher <joerg.hilscher@gmail.com>
2017-11-06 20:14:10 +0100

    Renamed to Fraction - Lots of added Feature to Fraction struct - Added more Tests

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,398 @@
+﻿namespace JoFraction
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    // IComparable, IComparable<Double>, IConvertible, IEquatable<Double>, IFormattable
+    public struct Fraction : IComparable<Fraction>, IEquatable<Fraction>, IFormattable
+    {
+        public const double DefaultPrecition = 10e-16;
+
+        public long Numerator { get; }
+
+        public long Denominator { get; }
+
+
+        /// <summary>
+        /// Creates a Fraction: [numerator]/[denominator]
+        /// </summary>
+        /// <param name="numerator">[x]/y</param>
+        /// <param name="denominator">x/[y]</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public Fraction(long numerator, long denominator)
+        {
+            if (denominator == 0)
+                throw new ArgumentOutOfRangeException(nameof(denominator), "Denominator is 0.");
+
+            if (numerator > 1 || denominator < -1)
+                (numerator, denominator) = Reduce(numerator, denominator);
+            
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        //public Fraction(double d, double precition = DefaultPrecition)
+        //{
+        //    var result = GetFraction(d, precition);
+
+        //    Numerator = result.numerator;
+        //    Denominator = result.denominator;
+        //}
+
+        /// <summary>
+        /// Creates a Fraction
+        /// </summary>
+        /// <example>
+        /// new Fraction((1, 2));
+        /// </example>
+        /// <remarks>
+        /// if you must use tuples
+        /// </remarks>
+        /// <param name="tuple">(long numerator, long denominator)</param>
+        public Fraction((long numerator, long denominator) tuple) : this (tuple.numerator, tuple.denominator)
+        { }
+   
+        /// <summary>
+        /// Copy Constructor
+        /// </summary>
+        /// <param name="f"></param>
+        public Fraction(Fraction f) : this(f.Numerator, f.Denominator)
+        { }
+
+
+        /// <summary>
+        /// Creates a <see cref="Fraction"/> from a string that contains a number or fraction.
+        /// </summary>
+        /// <param name="input">A string that contains a number or fraction to convert.</param>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static Fraction Parse(string s)
+        {
+            return new Fraction(InternalParse(s));
+        }
+
+        //public static bool TryParse(string s, out Fraction result)
+        //{
+        //    Double 
+        //}
+
+        private static (long numerator, long denominator) InternalParse(string s)
+        {
+            if (String.IsNullOrWhiteSpace(s))
+                throw new ArgumentNullException(nameof(s));
+
+            if (s.Contains("/"))
+            {
+                string[] values = s.Split('/', StringSplitOptions.None);
+
+                if (values.Length != 2)
+                    throw new FormatException($"Cannot convert {s} to {nameof(Fraction)}");
+
+                if (Int64.TryParse(values[0], out long numerator) &&
+                    Int64.TryParse(values[1], out long denominator))
+                {
+                    if (denominator == 0)
+                        throw new ArgumentOutOfRangeException("Denominator is 0.");
+
+                    return (numerator, denominator);
+                }
+
+                throw new FormatException($"Cannot convert {s} to {nameof(Fraction)}");
+            }
+
+            if (Double.TryParse(s, out double floatingPoint))
+            {
+                return GetFraction(floatingPoint, DefaultPrecition);
+            }
+            else
+            {
+                throw new FormatException($"Cannot convert {s} to {nameof(Fraction)}");
+            }
+        }
+
+        /// <summary>
+        /// Creates a Fraction from a Double.
+        /// </summary>
+        /// <remarks>
+        /// Stern–Brocot tree
+        /// </remarks>
+        /// <param name="d">Input Double</param>
+        /// <param name="precition">Precition when comparing doubles</param>
+        /// <returns>a new <see cref="Fraction"/></returns>
+        public static Fraction ToFraction(double d, double precition = DefaultPrecition)
+        {
+            return new Fraction(GetFraction(d, precition));
+        }
+
+        private static (long numerator, long denominator) GetFraction(double d, double precition)
+        {
+            long n = (long)Math.Floor(d);
+            d -= n;
+
+            if (d < precition)
+                return (n, 1);
+            else if ((1 - precition) < d)
+                return (n + 1, 1);
+
+            (long numerator, long denominator) lower = (0, 1);
+            (long numerator, long denominator) upper = (1, 1);
+
+            for (;;)
+            {
+                (long numerator, long denominator) middle = (lower.numerator + upper.numerator, lower.denominator + upper.denominator);
+
+                if (middle.denominator * (d + precition) < middle.numerator)
+                {
+                    upper = middle;
+                }
+                else if (middle.numerator < (d - precition) * middle.denominator)
+                {
+                    lower = middle;
+                }
+                else
+                {
+                    return (n * middle.denominator + middle.numerator, middle.denominator);
+                }
+            }
+        }
+
+        public double ToDouble()
+        {
+            return (double)Numerator / Denominator;
+        }
+
+        /// <summary>
+        /// Greatest common factor.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private static long ggT(long a, long b)
+        {
+            if (b == 0)
+                return a;
+            else return ggT(b, a % b);
+        }
+
+        private static long kgV(long a, long b)
+        {
+            return a / ggT(a, b) * b;
+        }
+
+        private static (long, long) Reduce(long numerator, long denominator)
+        {
+            long tmp = ggT(numerator, denominator);
+            denominator /= tmp;
+            numerator /= tmp;
+
+            if ((denominator < 0 && numerator > 0) || (denominator < 0 && numerator < 0))
+            {
+                denominator *= -1;
+                numerator *= -1;
+            }
+
+            return (numerator, denominator);
+        }
+
+        // Multiplizieren Basis Methode
+        public Fraction Multiply(Fraction other)
+        {
+            return new Fraction(Numerator * other.Numerator, Denominator * other.Denominator);
+        }
+
+        /// <summary>
+        /// Add this to other Fraction.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public Fraction Addition(Fraction other)
+        {
+            if (Numerator == 0)
+                return new Fraction(other);
+
+            if (other.Numerator == 0)
+                return new Fraction(this);
+
+            var ErgebnisNenner = kgV(Denominator, other.Denominator);
+            var ErgebnisZaehler = Numerator * (ErgebnisNenner / Denominator) +
+                other.Numerator * (ErgebnisNenner / other.Denominator);
+
+            return new Fraction(ErgebnisZaehler, ErgebnisNenner);
+        }
+
+        /// <summary>
+        /// Subtract this with other Fraction.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public Fraction Subtrahieren(Fraction other)
+        {
+            if (other.Numerator == 0)
+                return new Fraction(this);
+
+            var ErgebnisNenner = kgV(Denominator, other.Denominator);
+            var ErgebnisZaehler = Numerator * (ErgebnisNenner / Denominator) -
+                other.Numerator * (ErgebnisNenner / other.Denominator);
+            return new Fraction(ErgebnisZaehler, ErgebnisNenner);
+        }
+
+        /// <summary>
+        /// Divide this by other fraction.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>new Fraction as result</returns>
+        /// <exception cref="DivideByZeroException"></exception>
+        public Fraction Dividieren(Fraction other)
+        {
+            if (other.Numerator == 0)
+                throw new DivideByZeroException("other Fraction is zero.");
+
+            return new Fraction(Numerator * other.Denominator, Denominator * other.Numerator);
+        }
+
+        #region Operators
+
+        // Überladung '*' operator:
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            return a.Multiply(b);
+        }
+
+        // Überladung '/' operator:
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            return a.Dividieren(b);
+        }
+
+        // Überladung '+' operator:
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            return a.Addition(b);
+        }
+
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            return a.Subtrahieren(b);
+        }
+
+        public static bool operator ==(Fraction a, Fraction b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+            return !a.Equals(b);
+        }
+
+        public static bool operator <(Fraction a, Fraction b)
+        {
+            return a.CompareTo(b) < 0;
+        }
+
+        public static bool operator >(Fraction a, Fraction b)
+        {
+            return a.CompareTo(b) > 0;
+        }
+
+        public static bool operator <=(Fraction a, Fraction b)
+        {
+            return a.CompareTo(b) <= 0;
+        }
+
+        public static bool operator >=(Fraction a, Fraction b)
+        {
+            return a.CompareTo(b) >= 0;
+        }
+
+        #endregion
+
+        #region Converters
+
+        /// <summary>
+        /// Explicitly converts a <see cref="Fraction"/> to a <see cref="Double"/>.
+        /// </summary>
+        /// <example>
+        /// double d = (double)(new Fraction(1, 2));
+        /// </example>
+        public static explicit operator double(Fraction f)
+        {
+            return f.ToDouble();
+        }
+
+        /// <summary>
+        /// Explicitly converts a <see cref="Double"/> to a <see cref="Fraction"/>.
+        /// </summary>
+        /// <example>
+        /// Fraction d = (Fraction)1.213d;
+        /// </example>
+        public static explicit operator Fraction(double d)
+        {
+            return ToFraction(d);
+        }
+
+        /// <summary>
+        /// Tuple Deconstruction.
+        /// </summary>
+        /// <example>
+        /// var (n, d) = new Fraction(1, 2);
+        /// </example>
+        /// <param name="numerator">Numerator</param>
+        /// <param name="denominator">Denominator</param>
+        public void Deconstruct(out long numerator, out long denominator)
+        {
+            numerator = Numerator;
+            denominator = Denominator;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// IComparable implementation.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>-1, 1, 0</returns>
+        public int CompareTo(Fraction other)
+        {
+            if (this.Numerator <= other.Numerator && this.Denominator >= other.Denominator)
+                return -1;
+
+            if (this.Numerator >= other.Numerator && this.Denominator <= other.Denominator)
+                return 1;
+
+            return 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Fraction)
+            {
+                return Equals((Fraction)obj);
+            }
+
+            return false;
+        }
+
+        public bool Equals(Fraction other)
+        {
+            return other.Numerator == Numerator && other.Denominator == Denominator;
+        }
+
+        public override int GetHashCode()
+        {
+            return Denominator.GetHashCode() * 397 ^ Numerator.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return $"{Numerator.ToString(format, formatProvider)}/{Denominator.ToString(format, formatProvider)}";
+        }
+    }
+}
--- FractionTest/FractionTests.cs
+++ FractionTest/FractionTests.cs
@@ -1,33 +1,78 @@
 using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Fraction;
+using JoFraction;
 using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace FractionTest
 {
-    [TestClass]
+    [TestFixture]
     public class FractionTests
     {
 
-        [TestMethod]
+        [Test]
         public void CreateFractionTest()
         {
             // Arrange
 
             // Act
-            var newFraction = new F(4, 2);
+            var newFraction = new Fraction(4, 2);
 
             // Assert
             Assert.AreEqual(1, newFraction.Denominator);
             Assert.AreEqual(2, newFraction.Numerator);
         }
 
-        [TestMethod]
+        [Test]
+        public void AdditionTest1()
+        {
+            // Arrange
+            var a = new Fraction(5, 11);
+            var b = new Fraction(7, 3);
+
+            // Act
+            var result = a + b;
+
+            // Assert
+            Assert.AreEqual(33, result.Denominator);
+            Assert.AreEqual(92, result.Numerator);
+        }
+
+        [Test]
+        public void AdditionTest2()
+        {
+            // Arrange
+            var a = new Fraction(-5, 11);
+            var b = new Fraction(7, 3);
+
+            // Act
+            var result = a + b;
+
+            // Assert
+            Assert.AreEqual(33, result.Denominator);
+            Assert.AreEqual(62, result.Numerator);
+        }
+
+        [Test]
+        public void AdditionTest3()
+        {
+            // Arrange
+            var a = new Fraction(7, 11);
+            var b = new Fraction(0, 3);
+
+            // Act
+            var result = a + b;
+
+            // Assert
+            Assert.AreEqual(11, result.Denominator);
+            Assert.AreEqual(7, result.Numerator);
+        }
+
+        [Test]
         public void MultiplyTest()
         {
             // Arrange
-            var a = new F(5, 11);
-            var b = new F(7, 3);
+            var a = new Fraction(5, 11);
+            var b = new Fraction(7, 3);
 
             // Act
             var result = a * b;
@@ -37,12 +82,12 @@ namespace FractionTest
             Assert.AreEqual(35, result.Numerator);
         }
 
-        [TestMethod]
+        [Test]
         public void DevideTest()
         {
             // Arrange
-            var a = new F(5, 11);
-            var b = new F(7, 3);
+            var a = new Fraction(5, 11);
+            var b = new Fraction(7, 3);
 
             // Act
             var result = a / b;
@@ -52,12 +97,12 @@ namespace FractionTest
             Assert.AreEqual(15, result.Numerator);
         }
 
-        [TestMethod]
+        [Test]
         public void EqualsTest1()
         {
             // Arrange
-            var a = new F(5, 11);
-            var b = new F(7, 3);
+            var a = new Fraction(5, 11);
+            var b = new Fraction(7, 3);
 
             // Act
             var result = a == b;
@@ -66,12 +111,12 @@ namespace FractionTest
             Assert.IsFalse(result);
         }
 
-        [TestMethod]
+        [Test]
         public void EqualsTest2()
         {
             // Arrange
-            var a = new F(4, 2);
-            var b = new F(2, 1);
+            var a = new Fraction(4, 2);
+            var b = new Fraction(2, 1);
 
             // Act
             var result = a == b;
@@ -80,12 +125,12 @@ namespace FractionTest
             Assert.IsTrue(result);
         }
 
-        [TestMethod]
+        [Test]
         public void EqualsTest3()
         {
             // Arrange
-            var a = new F(3, 7);
-            var b = new F(3, 7);
+            var a = new Fraction(3, 7);
+            var b = new Fraction(3, 7);
 
             // Act
             var result = a == b;
@@ -94,12 +139,12 @@ namespace FractionTest
             Assert.IsTrue(result);
         }
 
-        [TestMethod]
+        [Test]
         public void ComparerTest1()
         {
             // Arrange
-            var a = new F(3, 7);
-            var b = new F(4, 7);
+            var a = new Fraction(3, 7);
+            var b = new Fraction(4, 7);
 
             // Act
             var result = a < b;
@@ -108,12 +153,12 @@ namespace FractionTest
             Assert.IsTrue(result);
         }
 
-        [TestMethod]
+        [Test]
         public void ComparerTest2()
         {
             // Arrange
-            var a = new F(3, 13);
-            var b = new F(3, 7);
+            var a = new Fraction(3, 13);
+            var b = new Fraction(3, 7);
 
             // Act
             var result = a < b;
@@ -122,15 +167,15 @@ namespace FractionTest
             Assert.IsTrue(result);
         }
 
-        [TestMethod]
+        [Test]
         public void SortTest1()
         {
             // Arrange
-            var a = new F(3, 13);
-            var b = new F(3, 7);
-            var c = new F(2, 7);
+            var a = new Fraction(3, 13);
+            var b = new Fraction(3, 7);
+            var c = new Fraction(2, 7);
 
-            var list = new List<F> { a, b, c };
+            var list = new List<Fraction> { a, b, c };
 
             // Act
             list.Sort();
@@ -141,11 +186,11 @@ namespace FractionTest
             Assert.AreEqual(list[2], b);
         }
 
-        [TestMethod]
+        [Test]
         public void ImplicidConverterTest1()
         {
             // Arrange
-            var a = new F(3, 2);
+            var a = new Fraction(3, 2);
 
             // Act
             double value = (double)a;
@@ -154,28 +199,28 @@ namespace FractionTest
             Assert.AreEqual(1.5D, value);
         }
 
-        [TestMethod]
+        [Test]
         public void ExplicidConverterTest2()
         {
             // Arrange
             double d = 5.0D;
 
             // Act
-            F f = (F)d;
+            Fraction f = (Fraction)d;
 
             // Assert
             Assert.AreEqual(5, f.Numerator);
             Assert.AreEqual(1, f.Denominator);
         }
 
-        [TestMethod]
+        [Test]
         public void DoubleToFractionTest1()
         {
             // Arrange
             double d = 5.125D;
 
             // Act
-            F f = F.ToFraction(d);
+            Fraction f = Fraction.ToFraction(d);
 
             Console.WriteLine(f);
 
@@ -184,39 +229,52 @@ namespace FractionTest
             Assert.AreEqual(8, f.Denominator);
         }
 
-        [TestMethod]
+        [Test]
         public void DoubleToFractionTest2()
         {
             // Arrange
             double d = -5.125D;
 
             // Act
-            F f = F.ToFraction(d, 0.000001);
+            Fraction f = Fraction.ToFraction(d, 0.000001);
 
             // Assert
             Assert.AreEqual(-41, f.Numerator);
             Assert.AreEqual(8, f.Denominator);
         }
 
-        [TestMethod]
+        [Test]
         public void DoubleToFractionTest3()
         {
             // Arrange
             double d = 0.01276993355481727574750830564784;
 
             // Act
-            F f = F.ToFraction(d, 0.000000000000000000000000000000000001);
+            Fraction f = Fraction.ToFraction(d, 10e-32);
 
             // Assert
             Assert.AreEqual(123, f.Numerator);
             Assert.AreEqual(9632, f.Denominator);
         }
 
-        [TestMethod]
+        [Test]
+        public void DoubleToFractionTest4()
+        {
+            // Arrange
+            double d = Math.PI; // Why not Pi?
+
+            // Act
+            Fraction f = Fraction.ToFraction(d, 10e-32);
+
+            // Assert
+            Assert.AreEqual(Math.PI, f.ToDouble());
+        }
+
+        [Test]
         public void FractionToTupleTest()
         {
             // Arrange
-            F f = new F(11,37);
+            Fraction f = new Fraction(11,37);
 
             // Act
             var (n, d) = f;
@@ -224,6 +282,171 @@ namespace FractionTest
             // Assert
             Assert.AreEqual(f.Numerator, n);
             Assert.AreEqual(f.Denominator, d);
+        }
+
+        [Test]
+        public void FractionEdgeTest1()
+        {
+            // Arrange
+            var a = new Fraction(0, 1);
+            var b = new Fraction(4, 1);
+
+            // Act
+            var c = a + b;
+
+            // Assert
+            Assert.AreEqual(b, c);
+        }
+
+        [Test]
+        public void FractionEdgeTest2()
+        {
+            // Arrange
+            var a = new Fraction(0, 1);
+            var b = new Fraction(4, 1);
+
+            // Act
+            var c = b - a;
+
+            // Assert
+            Assert.AreEqual(b, c);
+        }
+
+        [Test]
+        public void FractionEdgeTest3()
+        {
+            // Arrange
+            var a = new Fraction(0, 11);
+            var b = new Fraction(4, 1);
+
+            // Act
+            
+
+            // Assert
+            Assert.Throws<DivideByZeroException>(() => {
+                var c = b / a; // Devide by zero
+            });
+        }
+
+        [Test]
+        public void FractionFormattingTest()
+        {
+            // Arrange
+            var a = new Fraction(4521, 13);
+
+            // Act
+            string result = $"{a:N0}";
+
+            // Assert
+            Assert.AreEqual($"{4521:N0}/{13:N0}", result);
+        }
+
+        [Test]
+        public void FractionParseTest1()
+        {
+            // Arrange
+
+            // Act
+            var a = Fraction.Parse("4521/13");
+
+            // Assert
+            Assert.AreEqual(4521, a.Numerator);
+            Assert.AreEqual(13, a.Denominator);
+        }
+
+        [Test]
+        public void FractionParseTest2()
+        {
+            // Arrange
+
+            // Act
+            var a = Fraction.Parse("4521 /13");
+
+            // Assert
+            Assert.AreEqual(4521, a.Numerator);
+            Assert.AreEqual(13, a.Denominator);
+        }
+
+        [Test]
+        public void FractionParseTest3()
+        {
+            // Arrange
+
+            // Act
+            var a = Fraction.Parse("4521 /  13");
+
+            // Assert
+            Assert.AreEqual(4521, a.Numerator);
+            Assert.AreEqual(13, a.Denominator);
+        }
+
+        [Test]
+        public void FractionParseTest4()
+        {
+            // Arrange
+
+            // Act
+            var a = Fraction.Parse("4521 /  0013");
+
+            // Assert
+            Assert.AreEqual(4521, a.Numerator);
+            Assert.AreEqual(13, a.Denominator);
+        }
+
+        [Test]
+        public void FractionParseTest5()
+        {
+            // Arrange
+            double input = 13.12;
+
+            // Act
+            var a = Fraction.Parse(input.ToString());
+
+            // Assert
+            Assert.AreEqual(328, a.Numerator);
+            Assert.AreEqual(25, a.Denominator);
+        }
+
+        [Test]
+        public void ComplexTest1()
+        {
+            // Approx. Pi
+
+            // Arrange
+
+            // Act
+            Fraction sum = new Fraction(0, 1);
+
+            for (int i = 0; i < 10; i++)
+            {
+                sum += new Fraction((long)Math.Pow(-1, i + 1), 2 * i - 1);
+            }
+
+            sum *= (Fraction)4;
+
+            // Assert
+            Assert.AreEqual(3.41, sum.ToDouble());
+        }
+
+        [Test]
+        public void ComplexTest2()
+        {
+            // Arrange
+            Fraction c = new Fraction(1, 5);
+            var tmp = c;
+
+            // Act
+            for (int i = 0; i < 5; i++)
+            {
+                tmp = z(tmp);
+            }
+
+            // Assert
+            Assert.AreEqual(0.2, tmp.ToDouble(), "It is approx 0.2 -> 0.19999999999999999990384 ...");
+
+            Fraction z(Fraction zN) {
+                return zN * zN + c;
+            }
         }
     }
 }
